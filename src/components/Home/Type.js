578efdde2ac93fe103bf5de6905e07e4202367e5
nagendrasha191@gmail.com <nagendrasha191@gmail.com>
2023-01-20 22:19:41 +0530

    my-first-commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Full-Stack Developer",
+          "Freelancer",
+          "Python Programmer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
